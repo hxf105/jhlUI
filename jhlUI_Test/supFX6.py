6797e851779c53eb4541c873c29449245455359c
hxf105 <1053348103@qq.com>
2018-11-30 09:49:11 +0800

    封箱包装信息填写脚本编写

--- jhlUI_Test/supFX6.py
+++ jhlUI_Test/supFX6.py
@@ -12,3 +12,49 @@ findXpath(xpath='//*[@id="batch-flow-show"]/div/div/div[2]/div/ul/li[2]/div[3]/d
 wait3()
 findXpath(xpath='/html/body/div[2]/div[2]/div/div[2]/div/div/div/div/div/div/div[1]/div/div').click() #单击封箱信息按钮
 wait3()
+# 封第一个箱子
+findXpath(xpath='//*[@id="add_package_items"]/thead/tr/th[1]/div[1]/input').click() #单击全选按钮
+findXpath(xpath='//*[@id="add_package_items"]/tbody/tr[2]/td[6]/div/input').clear()
+findXpath(xpath='//*[@id="add_package_items"]/tbody/tr[2]/td[6]/div/input').send_keys('5') #修改封箱数量
+findXpath(xpath='/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[2]/div[1]/div[2]/div[4]/div[2]/ul/li[4]/a').clcik() #单击下一页按钮
+findXpath(xpath='//*[@id="add_package_items"]/thead/tr/th[1]/div[1]/input').click() #单击全选按钮
+findID(id='package-add-num').send_keys('1') #输入箱号
+findXpath(xpath='/html/body/div[3]/div[2]/div/div[3]/div[2]').click() #单击提交按钮
+# 封第二个箱子
+findXpath(xpath='//*[@id="add_package_items"]/thead/tr/th[1]/div[1]/input').click() #单击全选按钮
+findXpath(xpath='//*[@id="add_package_items"]/tbody/tr[2]/td[6]/div/input').clear()
+findXpath(xpath='//*[@id="add_package_items"]/tbody/tr[2]/td[6]/div/input').send_keys('5') #修改封箱数量
+findXpath(xpath='/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[2]/div[1]/div[2]/div[4]/div[2]/ul/li[4]/a').clcik() #单击下一页按钮
+findXpath(xpath='//*[@id="add_package_items"]/thead/tr/th[1]/div[1]/input').click() #单击全选按钮
+findID(id='package-add-num').send_keys('2') #输入箱号
+findXpath(xpath='/html/body/div[3]/div[2]/div/div[3]/div[2]').click() #单击提交按钮
+# findXpath(xpath='/html/body/div[3]/div[2]/div/div[1]/a/span[2]').click() #单击关闭按钮
+findXpath(xpath='/html/body/div[2]/div[2]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[2]/div[1]/div[2]/a[1]').click() #单击包装待添加按钮
+findID(id='package_type_num').clear() #清除箱号
+findID(id="package_type_num").send_keys('2-1') #输入新箱号
+findID(id='package_item_note').send_keys('配件') #输入货物说明
+findID(id='package_type_code').find_element_by_xpath("//option[@value='1']").click() #选择包装方式
+findID(id='package_type_id').find_element_by_xpath("//option[@value='A7AD3545-AD83-544B-8F36-22CC1E5FFE1B']") #选择包装尺寸
+findXpath(xpath='/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div/div[3]/div[2]/div[2]/div').click() #单击新增尺寸按钮
+findID(id='package-type-length').send_keys('1000') #输入长
+findID(id='package-type-width').send_keys('800') #输入宽
+findID(id='package-type-height').send_keys('500') #输入高
+findID(id='add-package-type').click() #单击确定按钮
+findID(id='package_suttle_weight').send_keys('100') #输入净重
+findID(id='package_rough_weight').send_keys('105') #输入毛重
+findXpath(xpath='/html/body/div[3]/div[2]/div/div[3]/div').click() #单击保存按钮
+findXpath(xpath='/html/body/div[2]/div[2]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[2]/div[2]/div[2]/a[1]/span').click() #单击包装待添加按钮
+findID(id='package_type_num').clear() #清除箱号
+findID(id="package_type_num").send_keys('1-1') #输入新箱号
+findID(id='package_item_note').send_keys('配件') #输入货物说明
+findID(id='package_type_code').find_element_by_xpath("//option[@value='1']").click() #选择包装方式
+findID(id='package_type_id').find_element_by_xpath("//option[@value='A7AD3545-AD83-544B-8F36-22CC1E5FFE1B']") #选择包装尺寸
+findXpath(xpath='/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div/div[3]/div[2]/div[2]/div').click() #单击新增尺寸按钮
+findID(id='package-type-length').send_keys('1000') #输入长
+findID(id='package-type-width').send_keys('800') #输入宽
+findID(id='package-type-height').send_keys('500') #输入高
+findID(id='add-package-type').click() #单击确定按钮
+findID(id='package_suttle_weight').send_keys('100') #输入净重
+findID(id='package_rough_weight').send_keys('105') #输入毛重
+findXpath(xpath='/html/body/div[3]/div[2]/div/div[3]/div').click() #单击保存按钮
+findXpath(xpath='/html/body/div[2]/div[2]/div/div[3]/div').click() #单击提交按钮
